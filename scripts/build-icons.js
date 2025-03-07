306cf7091530beea24d81f7554769e05784af9dc
emma <hi@emma.cafe>
2025-03-06 19:51:58 -0500

    modify icon build script to output multiple iconsets

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -10,61 +10,76 @@ import {
 import fs from "node:fs/promises";
 import path from "node:path";
 
-const iconSet = await importDirectory("icons");
+/**
+ *
+ * @param {import("@iconify/tools").IconSet} iconSet
+ * @param {string} iconSetName
+ * @param {{makeUniformColor?: boolean}} [options={}]
+ */
+const processIcons = async (iconSet, iconSetName, options = {}) => {
+  iconSet.forEach((name, type) => {
+    if (type !== "icon") {
+      return;
+    }
 
-iconSet.forEach((name, type) => {
-  if (type !== "icon") {
-    return;
-  }
+    const svg = iconSet.toSVG(name);
+    if (!svg) {
+      // Invalid icon
+      iconSet.remove(name);
+      return;
+    }
 
-  const svg = iconSet.toSVG(name);
-  if (!svg) {
-    // Invalid icon
-    iconSet.remove(name);
-    return;
-  }
+    // Clean up and optimize icons
+    try {
+      // Clean up icon code
+      cleanupSVG(svg);
 
-  // Clean up and optimize icons
-  try {
-    // Clean up icon code
-    cleanupSVG(svg);
+      // Assume icon is monotone: replace color with currentColor, add if missing
+      // If icon is not monotone, remove this code
+      parseColors(svg, {
+        defaultColor: "currentColor",
+        callback: options?.makeUniformColor
+          ? (attr, colorStr, color) => {
+              return !color || isEmptyColor(color) ? colorStr : "currentColor";
+            }
+          : undefined,
+      });
 
-    // Assume icon is monotone: replace color with currentColor, add if missing
-    // If icon is not monotone, remove this code
-    parseColors(svg, {
-      defaultColor: "currentColor",
-      // callback: (attr, colorStr, color) => {
-      //   return !color || isEmptyColor(color) ? colorStr : "currentColor";
-      // },
-    });
+      // Optimize
+      runSVGO(svg);
+    } catch (err) {
+      // Invalid icon
+      console.error(`Error parsing ${name}:`, err);
+      iconSet.remove(name);
+      return;
+    }
 
-    // Optimize
-    runSVGO(svg);
-  } catch (err) {
-    // Invalid icon
-    console.error(`Error parsing ${name}:`, err);
-    iconSet.remove(name);
-    return;
-  }
+    // Update icon
+    iconSet.fromSVG(name, svg);
+  });
 
-  // Update icon
-  iconSet.fromSVG(name, svg);
-});
+  // Target directory
+  const target = path.join("dist", "icons", iconSetName, iconSet.prefix);
 
-// Target directory
-const target = path.join("dist", "icons", iconSet.prefix);
+  // Export package
+  await exportJSONPackage(iconSet, {
+    target,
+    cleanup: true,
+  });
 
-// Export package
-await exportJSONPackage(iconSet, {
-  target,
-  cleanup: true,
-});
+  // A little hacky, but exportJSONPackage does everything we want, it just also
+  // writes a package.json that we don't need
+  await fs.rm(path.join(target, "package.json"));
 
-// A little hacky, but exportJSONPackage does everything we want, it just also
-// writes a package.json that we don't need
-await fs.rm(path.join(target, "package.json"));
+  await exportToDirectory(iconSet, {
+    target: `dist/icons/svg/${iconSetName}/${iconSet.prefix}`,
+    log: true,
+  });
+};
 
-await exportToDirectory(iconSet, {
-  target: `dist/icons/svg/${iconSet.prefix}`,
-  log: true,
-});
+await Promise.all([
+  processIcons(await importDirectory("iconsets/icons"), "icons"),
+  processIcons(await importDirectory("iconsets/brand"), "brand", {
+    makeUniformColor: true,
+  }),
+]);
